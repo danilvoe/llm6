3cdb524185d58350f84daadcc2021586d57e22b8
voe <voe@local.ru>
2025-08-16 08:39:33 +0300

    что то работает

---
+++ day6.py
@@ -0,0 +1,28 @@
+
+
+import asyncio
+
+from mcp import ClientSession
+from mcp.client.streamable_http import streamablehttp_client
+
+async def main():
+    async with streamablehttp_client("http://127.0.0.1:3001/mcp") as (
+        read_stream,
+        write_stream,
+        _,
+    ):
+        print("Конвертация веб страницы в markdown формат.")
+        url = input("Введите url для конвертации страницы в markdown формат: ").strip()
+        async with ClientSession(read_stream, write_stream) as session:
+            # Initialize the connection
+            await session.initialize()
+            # List available tools
+            tools = await session.list_tools()
+            print(f"Available tools: {[tool.name for tool in tools.tools]}")
+            tools = await session.call_tool('convert_to_markdown', {'uri': url})
+            file = open('result.md', 'w')
+            file.write(tools.content[0].text)
+            file.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
